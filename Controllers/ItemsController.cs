9109b5a2a2152264b210888a14377bb17bac47f7
Yash Pandey <ypandey777@yahoo.in>
2024-10-08 22:55:46 +0530

    first commit for Project setup

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()             //item/overview
+        {
+            var item = new Item() { Name = "Item1"};
+            return View(item);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"# ASP.Net-Core-MVC-.Net-9-" 
