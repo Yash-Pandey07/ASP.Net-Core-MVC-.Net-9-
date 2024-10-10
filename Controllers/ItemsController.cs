ee58b827e047b239fbca9cdc9e4b8cbe009e2da2
Yash Pandey <ypandey777@yahoo.in>
2024-10-11 00:24:09 +0530

    CRUD Reading the data & Creating the data

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyApp.Data;
 using MyApp.Models;
 
 namespace MyApp.Controllers
 {
     public class ItemsController : Controller
     {
+        /*
         //Action Result
         public IActionResult Overview()             //item/overview
         {
@@ -14,6 +17,35 @@ namespace MyApp.Controllers
         //Action Parameters
         public IActionResult Edit(int id) {         //http://localhost:7221/items/edit/5
             return Content("id = " +  id);          //http://localhost:7221/items/edit?id=5 
+        }
+        */
+
+        //CRUD operation
+        private readonly MyAppContext _context;
+        public ItemsController(MyAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var item = await _context.Items.ToListAsync();                 //making it async
+            return View(item);
+        }
+        public IActionResult Create() { 
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Price")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
         }
     }
 }
