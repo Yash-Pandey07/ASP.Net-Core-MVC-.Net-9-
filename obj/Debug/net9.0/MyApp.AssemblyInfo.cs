c4b2428a420e60a02018f477f861149b419a1208
Yash Pandey <ypandey777@yahoo.in>
2024-10-11 20:48:12 +0530

    Added Edit and Delete operations

--- obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e267267f88afd919015a053979339d6f8c116500")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee58b827e047b239fbca9cdc9e4b8cbe009e2da2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
